49c684615db5d3785ec06f9dc464e55bd9de584f
Clover <agusvidal.com@gmail.com>
2019-02-05 16:24:56 -0300

    Initial commit

---
+++ core/src/com/pacman/Actores/PacMan.java
@@ -0,0 +1,17 @@
+package com.pacman.Actores;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class PacMan extends Actor {
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+}
