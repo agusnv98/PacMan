2382ebe5a2150ab1cfd1fb40684db60e06b26a33
GastonVidart <gaston.vidart@gmail.com>
2019-02-17 16:22:45 -0300

    arreglo

--- android/src/com/pacman/AndroidLauncher.java
+++ android/src/com/pacman/AndroidLauncher.java
@@ -4,15 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.pacman.BaseDeDatos.BaseDeDatosAndroid;
-
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new JuegoPrincipal(new BaseDeDatosAndroid(getApplicationContext())), config);
-	}
 import com.pacman.Datos.BaseDeDatosAndroid;
 
 public class AndroidLauncher extends AndroidApplication {
--- core/src/com/pacman/BasedeDatos.java
+++ core/src/com/pacman/BasedeDatos.java
@@ -1,5 +1,12 @@
 package com.pacman;
 
-public interface BasedeDatos {
-    //aca hay que definir los metodos que va tener la base de datos
+public interface BaseDeDatos {
+
+    public void crearJugador(String nombre, String contraseña);
+
+    public void logIn(String nombre, String contraseña);
+
+    public void actualizarPuntaje(String nombre, int puntos);
+
+    public void mostrarDatos();
 }
